fca8490815146c4920e5bd70558d82564c92215d
justinnauta <justinnauta@users.noreply.github.com>
2020-05-29 01:45:12 -0400

    Cleaned up and further implemented header design

--- src/components/HeroHeader.vue
+++ src/components/HeroHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Page header ("First page") -->
   <div id="heroHeader">
-    <section class="hero is-fullheight is-primary">
+    <section class="hero is-fullheight is-light">
       <!-- Mobile navbar -->
       <div class="hero-head">
         <MainNav class="is-hidden-desktop"></MainNav>
@@ -9,17 +9,35 @@
       <!-- Content -->
       <div class="hero-body">
         <div class="container">
-          <div class="columns">
+          <!-- Two main columns -->
+          <div class="columns is-vcentered">
+            <!-- First Column -->
             <div class="column">
-              <div class="content is-medium">
-                <h1 class="title is-1">Hi, I'm Justin.</h1>
+              <div class="content is-medium has-text-dark">
+                <h1 class="title is-1 has-text-dark">Hi, I'm Justin.</h1>
                 <p>This will be a very quick summary of the content on the "About" page, or something similar.</p>
+                <div class="has-text-centered">
+                  <button class="button is-link is-rounded is-medium">Contact</button>
+                </div>
               </div>
             </div>
+            <!-- Second Column -->
             <div class="column">
-              <div class="content is-small">
-                <h3 class="title is-3">Featured Projects</h3>
-                <p>FEATURED PROJECTS HERE</p>
+              <h3 class="title is-3 has-text-dark">Featured Projects</h3>
+              <!-- Projects Columns -->
+              <div class="columns">
+                <div class="column">
+                  <ProjectCard></ProjectCard>
+                </div>
+                <div class="column">
+                  <ProjectCard></ProjectCard>
+                </div>
+              </div>
+              <div class="has-text-right">
+                <button class="button is-link is-rounded is-medium">
+                  View All
+                  <i class="fas fa-chevron-right"></i>
+                </button>
               </div>
             </div>
           </div>
@@ -35,11 +53,13 @@
 
 <script>
 import MainNav from "./MainNav.vue";
+import ProjectCard from "./ProjectCard.vue";
 
 export default {
   name: "HeroHeader",
   components: {
-    MainNav
+    MainNav,
+    ProjectCard
   }
 };
 </script>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="projectCard">
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-16by9">
+          <img src="../assets/media/notFound.png" alt="Project image" />
+        </figure>
+      </div>
+      <div class="card-content has-text-centered has-text-dark">
+        <p class="title is-5 has-text-centered has-text-dark">Title of Project</p>
+        <p class="is-size-6">A quick, one-liner description of the project.</p>
+        <button class="button is-link is-rounded" id="learnMoreBtn">Learn More</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectCard"
+};
+</script>
+
+<style>
+#learnMoreBtn {
+  margin-top: 1em;
+}
+</style>
