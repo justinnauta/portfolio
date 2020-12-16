d4b9a89f72cf46cdf96daa9910f7bd0093c7ff30
justinnauta <justinnauta@users.noreply.github.com>
2020-12-16 18:40:46 -0500

    Made all cards the same height

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,31 +1,26 @@
 <template>
   <!-- A card for displaying a project-->
-  <div>
-    <div class="card">
-      <!-- Project image -->
-      <div class="card-image">
-        <figure class="image is-16by9">
-          <img
-            :src="require(`@/assets/media/${project.imageNames[0]}`)"
-            alt="Project image"
-          />
-        </figure>
-      </div>
-      <!-- Card content -->
-      <div class="card-content has-text-centered has-text-dark">
-        <p class="title is-5 has-text-centered has-text-dark">
-          {{ project.title }}
-        </p>
-        <p class="is-size-6">
-          {{ project.oneLiner }}
-        </p>
-        <button
-          class="button is-link is-rounded mt-5"
-          @click="learnMoreClicked"
-        >
-          Learn More
-        </button>
-      </div>
+  <div class="card">
+    <!-- Project image -->
+    <div class="card-image">
+      <figure class="image is-16by9">
+        <img
+          :src="require(`@/assets/media/${project.imageNames[0]}`)"
+          alt="Project image"
+        />
+      </figure>
+    </div>
+    <!-- Card content -->
+    <div class="card-content has-text-centered has-text-dark">
+      <p class="title is-5 has-text-centered has-text-dark">
+        {{ project.title }}
+      </p>
+      <p class="is-size-6">
+        {{ project.oneLiner }}
+      </p>
+      <button class="button is-link is-rounded mt-5" @click="learnMoreClicked">
+        Learn More
+      </button>
     </div>
   </div>
 </template>
@@ -54,4 +49,7 @@ export default {
 </script>
 
 <style>
+.card, .card-content {
+  height: 100%;
+}
 </style>
