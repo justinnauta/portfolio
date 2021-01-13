b5d6f37fb7d6a4fa597c46cd449b6f20eb24a91b
justinnauta <justinnauta@users.noreply.github.com>
2021-01-13 18:27:00 -0500

    Finished contact page implementation

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -7,15 +7,68 @@
         <div class="container has-text-centered">
           <h2 class="title is-2 has-text-light">Contact Me</h2>
           <div class="columns">
-            <form id="contactForm" class="column is-half is-offset-one-quarter" @submit.prevent="submitForm">
+            <div class="column is-half is-offset-one-quarter">
+              <article
+                v-if="formResponse.message"
+                class="message"
+                :class="{
+                  'is-success': formResponse.success,
+                  'is-danger': !formResponse.success,
+                }"
+              >
+                <div
+                  class="message-body p-3 is-flex is-align-items-center is-justify-content-space-between"
+                >
+                  <p>{{ formResponse.message }}</p>
+                  <button
+                    class="delete ml-2"
+                    aria-label="delete"
+                    @click="closeFormResponse"
+                  ></button>
+                </div>
+              </article>
+            </div>
+          </div>
+          <div class="columns">
+            <form
+              class="column is-half is-offset-one-quarter"
+              @submit.prevent="submitForm"
+            >
               <!-- Input fields -->
-              <input type="text" class="input is-primary mb-3" placeholder="Name" name="name" v-model="name" />
-              <input type="email" class="input is-primary mb-3" placeholder="Email" name="email" v-model="email" />
-              <textarea class="textarea is-primary" rows="10" placeholder="Message" name="message" v-model="message"></textarea>
+              <input
+                type="text"
+                class="input is-primary mb-3"
+                placeholder="Name"
+                name="name"
+                v-model="formData.name"
+                required
+              />
+              <input
+                type="email"
+                class="input is-primary mb-3"
+                placeholder="Email"
+                name="email"
+                v-model="formData.email"
+                required
+              />
+              <textarea
+                class="textarea is-primary"
+                rows="10"
+                placeholder="Message"
+                name="message"
+                v-model="formData.message"
+                required
+              ></textarea>
               <div class="columns">
                 <div class="column is-4 is-offset-4">
                   <!-- Send button -->
-                  <button class="button is-link is-rounded is-medium is-fullwidth mt-3">Send</button>
+                  <button
+                    class="button is-link is-rounded is-medium is-fullwidth mt-3"
+                    :class="{ 'is-loading': loading }"
+                    :disabled="loading"
+                  >
+                    Send
+                  </button>
                 </div>
               </div>
             </form>
@@ -62,26 +115,59 @@
 </template>
 
 <script>
+const axios = require("axios");
+
 export default {
   name: "ContactPage",
-  data: function() {
+  data: function () {
     return {
-      name: '',
-      email: '',
-      message: ''
+      formData: {
+        name: null,
+        email: null,
+        message: null
+      },
+      formResponse: {
+        success: false,
+        message: null,
+      },
+      loading: false,
     };
   },
   methods: {
     submitForm: function () {
-      const formData = new FormData(document.querySelector("#contactForm"))
-      let xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-          console.log(this.responseText);
-        }
-      };
-      xhttp.open("POST", "../php/contact.php", true);
-      xhttp.send(formData);
+      // Check to prevent double clicks
+      if (this.loading) return;
+      else this.loading = true;
+
+      // Convert the formData variable to actual FormData for a POST
+      let data = new FormData();
+      for (let key in this.formData) {
+        data.append(key, this.formData[key]);
+      }
+
+      axios
+        .post("https://people.rit.edu/jrn2778/personal/php/contact.php", data)
+        .then((response) => {
+          this.formResponse.success = response.data.success;
+          this.formResponse.message = response.data.response;
+        })
+        .catch(() => {
+          this.formResponse.success = false;
+          this.formResponse.message =
+            "An unknown error has occurred. If the problem persists and you need to get in touch, feel free to contact me via LinkedIn.";
+        })
+        .then(() => {
+          this.loading = false;
+          if (this.formResponse.success) this.clearForm();
+        });
+    },
+    closeFormResponse: function () {
+      this.formResponse.message = null;
+    },
+    clearForm: function () {
+      for (let key in this.formData) {
+        this.formData[key] = null;
+      }
     },
   },
 };
